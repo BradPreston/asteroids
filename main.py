872389d443b2524ca37cba490cc62a44d4769907
bradpreston <bap5393@gmail.com>
2025-08-07 21:06:56 -0400

    Update main.py to add containers to Player class

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(
         x = SCREEN_WIDTH / 2,
         y = SCREEN_HEIGHT / 2
     )
-    updatable = pygame.sprite.Group(player)
-    drawable = pygame.sprite.Group(player)
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
